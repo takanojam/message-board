f1aa103502023170f248c9d15afaf992f1ecccd2
yoshiyukitakano <takanojam@gmail.com>
2016-05-05 11:41:02 +0000

    dbmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503121413) do
+ActiveRecord::Schema.define(version: 20160505102654) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
